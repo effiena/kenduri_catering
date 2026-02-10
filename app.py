4f9d69a78090eb14aa5ade15d3544f9c9c31d0aa
effiena <98937973+effiena@users.noreply.github.com>
2026-02-11 03:23:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -132,4 +132,5 @@ def vendor_logout():
     return redirect("/vendor/login")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
